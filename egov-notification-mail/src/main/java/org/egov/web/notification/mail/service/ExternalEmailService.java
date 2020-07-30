7b030b2b501ae863a402e20338765fe88cd80aaa
Tonmoy Tarafdar <tonmoy.tarafdar@pwc.com>
2020-07-30 14:36:42 +0530

    email attachment issue resolved.space is replaced by %20

--- egov-notification-mail/src/main/java/org/egov/web/notification/mail/service/ExternalEmailService.java
+++ egov-notification-mail/src/main/java/org/egov/web/notification/mail/service/ExternalEmailService.java
@@ -12,6 +12,7 @@ import org.egov.web.notification.mail.model.Email;
 import org.egov.web.notification.mail.model.EmailAttachment;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.core.io.InputStreamSource;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -27,18 +28,18 @@ public class ExternalEmailService implements EmailService {
 	public static final String EXCEPTION_MESSAGE = "Exception creating HTML email";
 	private JavaMailSenderImpl mailSender;
 
-    public ExternalEmailService(JavaMailSenderImpl mailSender) {
-        this.mailSender = mailSender;
-    }
+	public ExternalEmailService(JavaMailSenderImpl mailSender) {
+		this.mailSender = mailSender;
+	}
 
-    @Override
-    public void sendEmail(Email email) {
-		if(email.isHtml()) {
+	@Override
+	public void sendEmail(Email email) {
+		if (email.isHtml()) {
 			sendHTMLEmail(email);
 		} else {
 			sendTextEmail(email);
 		}
-    }
+	}
 
 	private void sendTextEmail(Email email) {
 		final SimpleMailMessage mailMessage = new SimpleMailMessage();
@@ -59,10 +60,11 @@ public class ExternalEmailService implements EmailService {
 			if (email.getAttachments() != null) {
 				for (EmailAttachment attachment : email.getAttachments()) {
 					InputStreamSource iss = new InputStreamSource() {
-						
+
 						@Override
 						public InputStream getInputStream() throws IOException {
-							return new BufferedInputStream(new URL(attachment.getUrl()).openStream());
+							return new BufferedInputStream(
+									new URL((attachment.getUrl()).replaceAll(" ", "%20")).openStream());
 						}
 					};
 					helper.addAttachment(attachment.getName(), iss, attachment.getMimeType());
@@ -72,6 +74,27 @@ public class ExternalEmailService implements EmailService {
 			log.error(EXCEPTION_MESSAGE, e);
 			throw new RuntimeException(e);
 		}
-		mailSender.send(message);
+		try {
+			mailSender.send(message);
+
+		} catch (MailException e) {
+
+			if (e.getMessage().toLowerCase().contains(("IOException").toLowerCase())
+					|| e.getMessage().toLowerCase().contains(("FileNotFoundException").toLowerCase())) {
+				MimeMessage newMessage = mailSender.createMimeMessage();
+				MimeMessageHelper newHelper;
+				try {
+					newHelper = new MimeMessageHelper(newMessage, true);
+					newHelper.setTo(email.getToAddress());
+					newHelper.setSubject(email.getSubject());
+					newHelper.setText((email.getBody()+ "<br> <p style='color:red;'>Note: Email attachment could not be uploaded.</p>"), true);
+				
+					mailSender.send(newMessage);
+				}catch(Exception ex) {
+					log.error(EXCEPTION_MESSAGE, ex);
+					throw new RuntimeException(ex);
+				}
+				}
+		}
 	}
 }
